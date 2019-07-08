2529efe4f2d12f1e61292d7f7df163a6378a0634
Landon <landon.baker@utexas.edu>
2019-07-08 01:13:15 -0500

    render updated item list upon state change

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -17,6 +17,13 @@ export default class Carousel extends React.Component {
       .then(() => this.getRelatedProductsWithImages(this.state.currentProduct.category));
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.currentProductId !== prevState.currentProductId) {
+      this.getCurrentProduct(this.state.currentProductId)
+        .then(() => this.getRelatedProductsWithImages(this.state.currentProduct.category));
+    }
+  }
+
   getCurrentProduct(prodID) {
     return axios.get(`http://localhost:3008/products/id?id=${prodID}`)
       .then(result => result.data)
@@ -44,8 +51,8 @@ export default class Carousel extends React.Component {
     return (
       <div>
         <h1 id="example">Carousel goes here</h1>
-        {/* <h4>{this.state.relatedProducts.length === 0 ? '' : this.state.relatedProducts[0].name}</h4>
-        <img width="100" height="100" src={this.state.images.length === 0 ? '#' : this.state.images[0].imageURL}></img> */}
+        <h4>{this.state.relatedProducts.length === 0 ? '' : this.state.relatedProducts[0].name}</h4>
+        {/* <img width="100" height="100" src={this.state.images.length === 0 ? '#' : this.state.images[0].imageURL}></img> */}
       </div>
     );
   }
